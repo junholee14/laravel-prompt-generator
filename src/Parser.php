aa0c0c084bafc0de48bd8bebf2621d160c3e71c3
junholee14 <junho2545@naver.com>
2024-04-17 16:32:20 +0900

    refactor

--- src/Parser.php
+++ src/Parser.php
@@ -22,27 +22,35 @@ class Parser
         $sourceCodes = [
             'route' => $route->getAction('controller')
         ];
+        $reflectionMethod = new \ReflectionMethod(...explode('@', $route->getAction('controller')));
+        $reflectionClass = new \ReflectionClass($reflectionMethod->class);
+        $sourceCodes[] = $this->extract($reflectionMethod, $reflectionClass);
         foreach ($promptDoc as $docComment) {
             $reflectionMethod = new \ReflectionMethod(...explode('::', $docComment));
             $reflectionClass = new \ReflectionClass($reflectionMethod->class);
-            $filename = $reflectionMethod->getFileName();
-            $startLine = $reflectionMethod->getStartLine() - 1;
-            $endLine = $reflectionMethod->getEndLine();
-            $length = $endLine - $startLine;
-
-            $source = file($filename);
-            $methodSourceCode = implode("", array_slice($source, $startLine, $length));
-            $docComment = $reflectionMethod->getDocComment();
-            $fileComment = $reflectionClass->getDocComment();
-
-            $sourceCodes[] = [
-                'filename' => $filename,
-                'sourceCode' => $methodSourceCode,
-                'fileComment' => $fileComment ?: '',
-                'methodComment' => $docComment ?: '',
-            ];
+            $sourceCodes[] = $this->extract($reflectionMethod, $reflectionClass);
         }
 
         return $sourceCodes;
+    }
+
+    private function extract(\ReflectionMethod $reflectionMethod, \ReflectionClass $reflectionClass)
+    {
+        $filename = $reflectionMethod->getFileName();
+        $startLine = $reflectionMethod->getStartLine() - 1;
+        $endLine = $reflectionMethod->getEndLine();
+        $length = $endLine - $startLine;
+
+        $source = file($filename);
+        $methodSourceCode = implode("", array_slice($source, $startLine, $length));
+        $fileComment = $reflectionClass->getDocComment();
+        $docComment = $reflectionMethod->getDocComment();
+
+        return [
+            'filename' => $filename,
+            'sourceCode' => $methodSourceCode,
+            'fileComment' => $fileComment ?: '',
+            'methodComment' => $docComment ?: '',
+        ];
     }
 }
